eed3a4d11a9fa7646acec420fa7a47fcac9b5c86
Dave Smith <smithdave@google.com>
2017-04-04 13:26:00 -0600

    Initial GATT Server Sample
    Implement a Time Profile Server using the
    Bluetooth GATT APIs.
    
    Change-Id: Iff0816f8921459f655cff6c6dda1292464d3b8a2
    Signed-off-by: Dave Smith <smithdave@google.com>

---
+++ app/src/main/java/com/example/androidthings/gattserver/GattServerActivity.java
@@ -0,0 +1,383 @@
+/*
+ * Copyright 2017, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.androidthings.gattserver;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattServer;
+import android.bluetooth.BluetoothGattServerCallback;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseData;
+import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.BluetoothLeAdvertiser;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.ParcelUuid;
+import android.util.Log;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class GattServerActivity extends Activity {
+    private static final String TAG = GattServerActivity.class.getSimpleName();
+
+    /* Bluetooth API */
+    private BluetoothGattServer mBluetoothGattServer;
+    private BluetoothLeAdvertiser mBluetoothLeAdvertiser;
+    /* Collection of notification subscribers */
+    private Set<BluetoothDevice> mRegisteredDevices = new HashSet<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        BluetoothManager manager =
+                (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        BluetoothAdapter bluetoothAdapter = manager.getAdapter();
+        // We can't continue without proper Bluetooth support
+        if (!checkBluetoothSupport(bluetoothAdapter)) {
+            finish();
+        }
+
+        // Register for system Bluetooth events
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(mBluetoothReceiver, filter);
+        if (!bluetoothAdapter.isEnabled()) {
+            Log.d(TAG, "Bluetooth is currently disabled...enabling");
+            bluetoothAdapter.enable();
+        } else {
+            Log.d(TAG, "Bluetooth enabled...starting services");
+            startAdvertising(manager);
+            startServer(manager);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Register for system clock events
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_TIME_TICK);
+        filter.addAction(Intent.ACTION_TIME_CHANGED);
+        filter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
+        registerReceiver(mTimeReceiver, filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(mTimeReceiver);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        BluetoothManager manager =
+                (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        BluetoothAdapter bluetoothAdapter = manager.getAdapter();
+        if (bluetoothAdapter.isEnabled()) {
+            stopServer();
+            stopAdvertising();
+        }
+
+        unregisterReceiver(mBluetoothReceiver);
+    }
+
+    /**
+     * Verify the level of Bluetooth support provided by the hardware.
+     * @param bluetoothAdapter System {@link BluetoothAdapter}.
+     * @return true if Bluetooth is properly supported, false otherwise.
+     */
+    private boolean checkBluetoothSupport(BluetoothAdapter bluetoothAdapter) {
+
+        if (bluetoothAdapter == null) {
+            Log.w(TAG, "Bluetooth is not supported");
+            return false;
+        }
+
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Log.w(TAG, "Bluetooth LE is not supported");
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Listens for system time changes and triggers a notification to
+     * Bluetooth subscribers.
+     */
+    private BroadcastReceiver mTimeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int adjustReason;
+            switch (intent.getAction()) {
+                case Intent.ACTION_TIME_CHANGED:
+                    adjustReason = TimeProfile.ADJUST_MANUAL;
+                    break;
+                case Intent.ACTION_TIMEZONE_CHANGED:
+                    adjustReason = TimeProfile.ADJUST_TIMEZONE;
+                    break;
+                default:
+                case Intent.ACTION_TIME_TICK:
+                    adjustReason = TimeProfile.ADJUST_NONE;
+                    break;
+            }
+            notifyRegisteredDevices(adjustReason);
+        }
+    };
+
+    /**
+     * Listens for Bluetooth adapter events to enable/disable
+     * advertising and server functionality.
+     */
+    private BroadcastReceiver mBluetoothReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.STATE_OFF);
+
+            BluetoothManager manager =
+                    (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+            switch (state) {
+                case BluetoothAdapter.STATE_ON:
+                    startAdvertising(manager);
+                    startServer(manager);
+                    break;
+                case BluetoothAdapter.STATE_OFF:
+                    stopServer();
+                    stopAdvertising();
+                    break;
+                default:
+                    // Do nothing
+            }
+
+        }
+    };
+
+    /**
+     * Begin advertising over Bluetooth that this device is connectable
+     * and supports the Current Time Service.
+     */
+    private void startAdvertising(BluetoothManager bluetoothManager) {
+        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+        mBluetoothLeAdvertiser = bluetoothAdapter.getBluetoothLeAdvertiser();
+        if (mBluetoothLeAdvertiser == null) {
+            Log.w(TAG, "Failed to create advertiser");
+            return;
+        }
+
+        AdvertiseSettings settings = new AdvertiseSettings.Builder()
+                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
+                .setConnectable(true)
+                .setTimeout(0)
+                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
+                .build();
+
+        AdvertiseData data = new AdvertiseData.Builder()
+                .setIncludeDeviceName(true)
+                .setIncludeTxPowerLevel(false)
+                .addServiceUuid(new ParcelUuid(TimeProfile.TIME_SERVICE))
+                .build();
+
+        mBluetoothLeAdvertiser
+                .startAdvertising(settings, data, mAdvertiseCallback);
+    }
+
+    /**
+     * Stop Bluetooth advertisements.
+     */
+    private void stopAdvertising() {
+        if (mBluetoothLeAdvertiser == null) return;
+
+        mBluetoothLeAdvertiser.stopAdvertising(mAdvertiseCallback);
+    }
+
+    /**
+     * Initialize the GATT server instance with the services/characteristics
+     * from the Time Profile.
+     */
+    private void startServer(BluetoothManager bluetoothManager) {
+        mBluetoothGattServer = bluetoothManager.openGattServer(this, mGattServerCallback);
+        if (mBluetoothGattServer == null) {
+            Log.w(TAG, "Unable to create GATT server");
+            return;
+        }
+
+        mBluetoothGattServer.addService(TimeProfile.createTimeService());
+    }
+
+    /**
+     * Shut down the GATT server.
+     */
+    private void stopServer() {
+        if (mBluetoothGattServer == null) return;
+
+        mBluetoothGattServer.close();
+    }
+
+    /**
+     * Callback to receive information about the advertisement process.
+     */
+    private AdvertiseCallback mAdvertiseCallback = new AdvertiseCallback() {
+        @Override
+        public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+            Log.i(TAG, "LE Advertise Started.");
+        }
+
+        @Override
+        public void onStartFailure(int errorCode) {
+            Log.w(TAG, "LE Advertise Failed: "+errorCode);
+        }
+    };
+
+    /**
+     * Send a time service notification to any devices that are subscribed
+     * to the characteristic.
+     */
+    public void notifyRegisteredDevices(int adjustReason) {
+        if (mRegisteredDevices.isEmpty()) {
+            Log.i(TAG, "No subscribers registered");
+            return;
+        }
+        byte[] exactTime = TimeProfile.getExactTime(System.currentTimeMillis(), adjustReason);
+
+        Log.i(TAG, "Sending update to " + mRegisteredDevices.size() + " subscribers");
+        for (BluetoothDevice device : mRegisteredDevices) {
+            BluetoothGattCharacteristic timeCharacteristic = mBluetoothGattServer
+                    .getService(TimeProfile.TIME_SERVICE)
+                    .getCharacteristic(TimeProfile.CURRENT_TIME);
+            timeCharacteristic.setValue(exactTime);
+            mBluetoothGattServer.notifyCharacteristicChanged(device, timeCharacteristic, false);
+        }
+    }
+
+    /**
+     * Callback to handle incoming requests to the GATT server.
+     * All read/write requests for characteristics and descriptors are handled here.
+     */
+    private BluetoothGattServerCallback mGattServerCallback = new BluetoothGattServerCallback() {
+
+        @Override
+        public void onConnectionStateChange(BluetoothDevice device, int status, int newState) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                Log.i(TAG, "BluetoothDevice CONNECTED: " + device);
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                Log.i(TAG, "BluetoothDevice DISCONNECTED: " + device);
+            }
+        }
+
+        @Override
+        public void onCharacteristicReadRequest(BluetoothDevice device, int requestId, int offset,
+                                                BluetoothGattCharacteristic characteristic) {
+            long now = System.currentTimeMillis();
+            if (TimeProfile.CURRENT_TIME.equals(characteristic.getUuid())) {
+                Log.i(TAG, "Read CurrentTime");
+                mBluetoothGattServer.sendResponse(device,
+                        requestId,
+                        BluetoothGatt.GATT_SUCCESS,
+                        0,
+                        TimeProfile.getExactTime(now, TimeProfile.ADJUST_NONE));
+            } else if (TimeProfile.LOCAL_TIME_INFO.equals(characteristic.getUuid())) {
+                Log.i(TAG, "Read LocalTimeInfo");
+                mBluetoothGattServer.sendResponse(device,
+                        requestId,
+                        BluetoothGatt.GATT_SUCCESS,
+                        0,
+                        TimeProfile.getLocalTimeInfo(now));
+            } else {
+                // Invalid characteristic
+                Log.w(TAG, "Invalid Characteristic Read: " + characteristic.getUuid());
+                mBluetoothGattServer.sendResponse(device,
+                        requestId,
+                        BluetoothGatt.GATT_FAILURE,
+                        0,
+                        null);
+            }
+        }
+
+        @Override
+        public void onDescriptorReadRequest(BluetoothDevice device, int requestId, int offset,
+                                            BluetoothGattDescriptor descriptor) {
+            if (TimeProfile.CLIENT_CONFIG.equals(descriptor.getUuid())) {
+                Log.d(TAG, "Config descriptor read");
+                byte[] returnValue;
+                if (mRegisteredDevices.contains(device)) {
+                    returnValue = BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE;
+                } else {
+                    returnValue = BluetoothGattDescriptor.DISABLE_NOTIFICATION_VALUE;
+                }
+                mBluetoothGattServer.sendResponse(device,
+                        requestId,
+                        BluetoothGatt.GATT_FAILURE,
+                        0,
+                        returnValue);
+            } else {
+                Log.w(TAG, "Unknown descriptor read request");
+                mBluetoothGattServer.sendResponse(device,
+                        requestId,
+                        BluetoothGatt.GATT_FAILURE,
+                        0,
+                        null);
+            }
+        }
+
+        @Override
+        public void onDescriptorWriteRequest(BluetoothDevice device, int requestId,
+                                             BluetoothGattDescriptor descriptor,
+                                             boolean preparedWrite, boolean responseNeeded,
+                                             int offset, byte[] value) {
+            if (TimeProfile.CLIENT_CONFIG.equals(descriptor.getUuid())) {
+                if (Arrays.equals(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE, value)) {
+                    Log.d(TAG, "Subscribe device to notifications: " + device);
+                    mRegisteredDevices.add(device);
+                } else if (Arrays.equals(BluetoothGattDescriptor.DISABLE_NOTIFICATION_VALUE, value)) {
+                    Log.d(TAG, "Unsubscribe device from notifications: " + device);
+                    mRegisteredDevices.remove(device);
+                }
+
+                if (responseNeeded) {
+                    mBluetoothGattServer.sendResponse(device,
+                            requestId,
+                            BluetoothGatt.GATT_SUCCESS,
+                            0,
+                            null);
+                }
+            } else {
+                Log.w(TAG, "Unknown descriptor write request");
+                if (responseNeeded) {
+                    mBluetoothGattServer.sendResponse(device,
+                            requestId,
+                            BluetoothGatt.GATT_FAILURE,
+                            0,
+                            null);
+                }
+            }
+        }
+    };
+}
---
+++ app/src/main/java/com/example/androidthings/gattserver/TimeProfile.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright 2017, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.androidthings.gattserver;
+
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
+
+import java.util.Calendar;
+import java.util.UUID;
+
+/**
+ * Implementation of the Bluetooth GATT Time Profile.
+ * https://www.bluetooth.com/specifications/adopted-specifications
+ */
+public class TimeProfile {
+    private static final String TAG = TimeProfile.class.getSimpleName();
+
+    /* Current Time Service UUID */
+    public static UUID TIME_SERVICE = UUID.fromString("00001805-0000-1000-8000-00805f9b34fb");
+    /* Mandatory Current Time Information Characteristic */
+    public static UUID CURRENT_TIME    = UUID.fromString("00002a2b-0000-1000-8000-00805f9b34fb");
+    /* Optional Local Time Information Characteristic */
+    public static UUID LOCAL_TIME_INFO = UUID.fromString("00002a0f-0000-1000-8000-00805f9b34fb");
+    /* Mandatory Client Characteristic Config Descriptor */
+    public static UUID CLIENT_CONFIG = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
+
+    // Adjustment Flags
+    public static final byte ADJUST_NONE     = 0x0;
+    public static final byte ADJUST_MANUAL   = 0x1;
+    public static final byte ADJUST_EXTERNAL = 0x2;
+    public static final byte ADJUST_TIMEZONE = 0x4;
+    public static final byte ADJUST_DST      = 0x8;
+
+    /**
+     * Return a configured {@link BluetoothGattService} instance for the
+     * Current Time Service.
+     */
+    public static BluetoothGattService createTimeService() {
+        BluetoothGattService service = new BluetoothGattService(TIME_SERVICE,
+                BluetoothGattService.SERVICE_TYPE_PRIMARY);
+
+        // Current Time characteristic
+        BluetoothGattCharacteristic currentTime = new BluetoothGattCharacteristic(CURRENT_TIME,
+                //Read-only characteristic, supports notifications
+                BluetoothGattCharacteristic.PROPERTY_READ | BluetoothGattCharacteristic.PROPERTY_NOTIFY,
+                BluetoothGattCharacteristic.PERMISSION_READ);
+        BluetoothGattDescriptor configDescriptor = new BluetoothGattDescriptor(CLIENT_CONFIG,
+                //Read/write descriptor
+                BluetoothGattDescriptor.PERMISSION_READ | BluetoothGattDescriptor.PERMISSION_WRITE);
+        currentTime.addDescriptor(configDescriptor);
+
+        // Local Time Information characteristic
+        BluetoothGattCharacteristic localTime = new BluetoothGattCharacteristic(LOCAL_TIME_INFO,
+                //Read-only characteristic
+                BluetoothGattCharacteristic.PROPERTY_READ,
+                BluetoothGattCharacteristic.PERMISSION_READ);
+
+        service.addCharacteristic(currentTime);
+        service.addCharacteristic(localTime);
+
+        return service;
+    }
+
+    /**
+     * Return the current time as an Exact Time 256 Field
+     * for the current time characteristic.
+     */
+    public static byte[] getExactTime(long timestamp, int adjustReason) {
+        Calendar time = Calendar.getInstance();
+        time.setTimeInMillis(timestamp);
+
+        byte[] field = new byte[10];
+
+        // Year
+        int year = time.get(Calendar.YEAR);
+        field[0] = (byte) (year & 0xFF);
+        field[1] = (byte) ((year >> 8) & 0xFF);
+        // Month
+        field[2] = (byte) ((time.get(Calendar.MONTH) + 1) & 0xFF);
+        // Day
+        field[3] = (byte) (time.get(Calendar.DATE) & 0xFF);
+        // Hours
+        field[4] = (byte) (time.get(Calendar.HOUR_OF_DAY) & 0xFF);
+        // Minutes
+        field[5] = (byte) (time.get(Calendar.MINUTE) & 0xFF);
+        // Seconds
+        field[6] = (byte) (time.get(Calendar.SECOND) & 0xFF);
+        // Day of Week (1-7)
+        field[7] = getDayOfWeekCode(time.get(Calendar.DAY_OF_WEEK));
+        // Fractions256
+        field[8] = (byte) ((time.get(Calendar.MILLISECOND) / 256) & 0xFF);
+
+        field[9] = (byte) (adjustReason & 0xFF);
+
+        return field;
+    }
+
+    /* Time bucket constants for local time information */
+    private static final int FIFTEEN_MINUTE_MILLIS = 900000;
+    private static final int ONE_HOUR_MILLIS = 3600000;
+
+    /**
+     * Return the time zone and DST offset fields that make up
+     * the local time information characteristic.
+     */
+    public static byte[] getLocalTimeInfo(long timestamp) {
+        Calendar time = Calendar.getInstance();
+        time.setTimeInMillis(timestamp);
+
+        byte[] field = new byte[2];
+
+        // Time zone
+        int rawOffset = time.get(Calendar.ZONE_OFFSET) / FIFTEEN_MINUTE_MILLIS; // 15 minute increments
+        field[0] = (byte) rawOffset;
+
+        // DST Offset
+        rawOffset = time.get(Calendar.DST_OFFSET) / ONE_HOUR_MILLIS;
+        field[1] = getDstOffsetCode(rawOffset);
+
+        return field;
+    }
+
+    /* Bluetooth Weekday Codes */
+    private static final byte DAY_UNKNOWN = 0;
+    private static final byte DAY_MONDAY = 1;
+    private static final byte DAY_TUESDAY = 2;
+    private static final byte DAY_WEDNESDAY = 3;
+    private static final byte DAY_THURSDAY = 4;
+    private static final byte DAY_FRIDAY = 5;
+    private static final byte DAY_SATURDAY = 6;
+    private static final byte DAY_SUNDAY = 7;
+
+    /**
+     * Convert a {@link Calendar} weekday value to the corresponding
+     * Bluetooth weekday code.
+     */
+    private static byte getDayOfWeekCode(int dayOfWeek) {
+        switch (dayOfWeek) {
+            case Calendar.MONDAY:
+                return DAY_MONDAY;
+            case Calendar.TUESDAY:
+                return DAY_TUESDAY;
+            case Calendar.WEDNESDAY:
+                return DAY_WEDNESDAY;
+            case Calendar.THURSDAY:
+                return DAY_THURSDAY;
+            case Calendar.FRIDAY:
+                return DAY_FRIDAY;
+            case Calendar.SATURDAY:
+                return DAY_SATURDAY;
+            case Calendar.SUNDAY:
+                return DAY_SUNDAY;
+            default:
+                return DAY_UNKNOWN;
+        }
+    }
+
+    /* Bluetooth DST Offset Codes */
+    private static final byte DST_STANDARD = 0x0;
+    private static final byte DST_HALF     = 0x2;
+    private static final byte DST_SINGLE   = 0x4;
+    private static final byte DST_DOUBLE   = 0x8;
+    private static final byte DST_UNKNOWN = (byte) 0xFF;
+
+    /**
+     * Convert a raw DST offset (in hours) to the corresponding
+     * Bluetooth DST offset code.
+     */
+    private static byte getDstOffsetCode(int rawOffset) {
+        switch (rawOffset) {
+            case 0:
+                return DST_STANDARD;
+            case 1:
+                return DST_SINGLE;
+            case 2:
+                return DST_DOUBLE;
+            default:
+                return DST_UNKNOWN;
+        }
+    }
+}
